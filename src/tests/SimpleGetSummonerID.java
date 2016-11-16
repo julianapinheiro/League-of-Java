2fb24980a5e93a8ca6921b5614023ef65d42fcf7
Juliana <julianas.pinheiro@outlook.com>
2016-11-16 10:56:05 -0200

    Testing method to get free week

--- src/tests/SimpleGetSummonerID.java
+++ src/tests/SimpleGetSummonerID.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import net.rithms.riot.api.RiotApi;
 import net.rithms.riot.api.RiotApiException;
 import net.rithms.riot.constant.Region;
+import net.rithms.riot.dto.Champion.Champion;
+import net.rithms.riot.dto.Champion.ChampionList;
 import net.rithms.riot.dto.Summoner.Summoner;
 //import com.google.gson.*;
 
@@ -14,16 +16,24 @@ public class SimpleGetSummonerID {
 	 * Just a simple class for learning how the wrapper works
 	 */
 	
-	public SimpleGetSummonerID(RiotApi api) {
-
+	private RiotApi api;
+	
+	public SimpleGetSummonerID(RiotApi api) throws RiotApiException{
+		this.api = api;
+		
         Map<String, Summoner> summoners;
-		try {
-			summoners = api.getSummonersByName(Region.BR, "theaan");
-			Summoner summoner = summoners.get("theaan");
-	        long id = summoner.getId();
-	        System.out.println(id);
-		} catch (RiotApiException e) {
-			e.printStackTrace();
-		}
+		summoners = api.getSummonersByName(Region.BR, "theaan");
+		Summoner summoner = summoners.get("theaan");
+	    long id = summoner.getId();
+	    System.out.println(id);
+	     
+	}
+	
+	public void GetFreeWeekChampions() throws RiotApiException{
+		ChampionList lista = api.getChampions(Region.BR, true);
+        for (Champion champ : lista.getChampions()) {
+        	long id = champ.getId();
+        	System.out.println(api.getDataChampion((int) id).getName());
+        }
 	}
 }
